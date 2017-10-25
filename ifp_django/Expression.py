413514203f7eebdde1224180ae31dec8830519df
David Wild <dwild@bedfordschool.org.uk>
2017-10-25 13:40:52 +0100

    Added some silly print statement

--- ifp_django/Expression.py
+++ ifp_django/Expression.py
@@ -17,6 +17,7 @@ import Randoms
 from definitions import tokenRandomiser, tokenRandomRange, tokenRandomString, \
     tokenEvaluate, tokenScientificFunctionName
 
+print('tobeornottobe')
 
 class NoPictureException(Exception):
     pass
